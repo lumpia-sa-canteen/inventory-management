8fed1752ce6cbe2a4dce7e526891e006047e9232
John Benedict Bere <john.bere212@gmail.com>
2026-02-21 09:38:08 +0800

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using SiyaLangAngGustoKo.Views;
+
+namespace SiyaLangAngGustoKo
+{
+    class Program
+    {
+        static void Main(String [] args)
+        {
+            InventoryView iv = new InventoryView();
+            iv.run();
+        }
+    }
+}
